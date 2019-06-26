726ff99390e0eada89c1058f389f8123c2ed95f4
Okamoto <Takamitsu.Okamoto@sony.com>
2019-06-26 09:23:02 +0900

    update

--- src/main/java/todo/interfaces/HelloWorldController.java
+++ src/main/java/todo/interfaces/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping
 	public String index() {
-		return "Hello World!";
+		return "Welcome to AWS World!";
 	}
 }
